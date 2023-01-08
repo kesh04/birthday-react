8cea25d9c18aadee12cd6ffcbd190611532b3662
keshan <keshana004@gmail.com>
2023-01-08 20:56:58 +0530

    birthday reminder

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      name: 'keshana',
+      age: 19,
+      image:
+        'https://lh3.googleusercontent.com/a/AEdFTp4PTHJFWDKYIw24oXGPn5vgBFQYOxXoimq3EscXjg=s360-p-rw-no'
+    },
+    {
+      id: 2,
+      name: 'sanjaya',
+      age: 23,
+      image:
+        'https://pps.whatsapp.net/v/t61.24694-24/312259800_867621934554114_3276022591922434390_n.jpg?ccb=11-4&oh=01_AdQ0yQrciui74pnpGqTm8RvHbcsuHpd4IANomMbFhrIvNw&oe=63C6F02C',
+    },
+    {
+      id: 3,
+      name: 'sahan',
+      age: 18,
+      image:
+        'https://pps.whatsapp.net/v/t61.24694-24/312285942_1327137508126156_6833138601514953652_n.jpg?ccb=11-4&oh=01_AdROMkplWVJr27O1qAgr7z3WW8x-6frqtIhtVVktTSWbyA&oe=63C6F79F',
+    },
+    {
+      id: 4,
+      name: 'keshara',
+      age: 24,
+      image:
+        'https://pps.whatsapp.net/v/t61.24694-24/320869747_2131573513716814_7978926967167242051_n.jpg?ccb=11-4&oh=01_AdTSn8GqSoX6GMyGZ0vLXBa3U027lwVkzy0_BSASUFVFmw&oe=63C6DB4B',
+    },
+    {
+      id: 5,
+      name: 'pasidu',
+      age: 26,
+      image:
+        'https://pps.whatsapp.net/v/t61.24694-24/312199820_1119250305621698_3089741531021634987_n.jpg?ccb=11-4&oh=01_AdQhUMEzvJZ1jTeol5rZ9RDzqEnyHnEiu_BxU6iW3_CM8A&oe=63C6D889',
+    },
+  ];
+  
